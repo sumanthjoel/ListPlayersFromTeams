8bb6c9daa47f3de76bd33a61bd0178a3b94c2025
Sumanth Joel <joel.1516@gmail.com>
2019-08-14 17:20:21 +0200

    Update main.go

--- main.go
+++ main.go
@@ -6,95 +6,122 @@ import (
 	"golang.org/x/text/collate"
 	"golang.org/x/text/language"
 	"io/ioutil"
-	"math"
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 )
 
-type player struct {
+// Player contains detailed information of a player from a team
+// We only read information that we need
+type Player struct {
 	ID   string `json:"id"`
 	Age  string `json:"Age"`
 	Name string `json:"name"`
 }
 
-type team struct {
-	ID      uint64   `json:"id"`
+// TeamDetails contains the detailed information about each team.
+// We only read information that we require.
+type TeamDetails struct {
+	ID      uint     `json:"id"`
 	Name    string   `json:"name"`
-	Players []player `json:"players"`
+	Players []Player `json:"players"`
 }
 
-type data struct {
-	Team team `json:"team"`
+// Data is the required information from APIResponse
+type Data struct {
+	TeamDetails TeamDetails `json:"team"`
 }
 
-type teamDetails struct {
+// APIResponse is the content read from the API endpoint
+type APIResponse struct {
 	Status  string `json:"status"`
 	Code    int    `json:"code"`
 	Message string `json:"message"`
-	Data    data   `json:"data"`
+	Data    Data   `json:"data"`
 }
 
+// playerDetails is the value for the map of required players' information
+// players = make(map[string]playerDetails)
 type playerDetails struct {
 	ID    string   `json:"id"`
 	Age   string   `json:"age"`
 	Teams []string `json:"teams"`
 }
 
+// Mutex to safely read the map concurrently
+var (
+	players map[string]playerDetails
+	lock    sync.RWMutex
+)
+
 func main() {
+	var wg sync.WaitGroup
 	requiredTeams := []string{"Germany", "England", "France", "Spain", "Manchester United", "Arsenal", "Chelsea", "Barcelona", "Real Madrid", "Bayern Munich"}
-	players := make(map[string]playerDetails)
+	players = make(map[string]playerDetails)
 	var playerNames []string
-	var count uint
-	count = math.MaxUint32
+	// identify the maximum value for uint
+	const MaxUint = ^uint(0)
 	var i uint
-	var j int
-	for i = 1; i < count && j < len(requiredTeams); i++ {
+	var noOfTeamsFound int
+	// When noOfTeamsFound matches len(requiredTeams), we got all we need
+	for ; i < MaxUint && noOfTeamsFound < len(requiredTeams); i++ {
 		resp, err := http.Get("https://vintagemonster.onefootball.com/api/teams/en/" + fmt.Sprint(i) + ".json")
 		if err != nil {
-			//log.Println(err)
 			continue
 		} else if resp.StatusCode != 200 {
-			//log.Println("Response is not valid: ", resp.StatusCode)
 			continue
 		}
 		defer resp.Body.Close()
-		var teamDetails teamDetails
+		var apiResponse APIResponse
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			//log.Println(err)
 			continue
 		}
-		err = json.Unmarshal(body, &teamDetails)
+		err = json.Unmarshal(body, &apiResponse)
 		if err != nil {
-			//log.Println(err)
 			continue
 		}
-		if !contains(requiredTeams, teamDetails.Data.Team.Name) {
+		if !contains(requiredTeams, apiResponse.Data.TeamDetails.Name) {
 			continue
 		}
-		j++
-		for _, teamPlayer := range teamDetails.Data.Team.Players {
-			if val, ok := players[teamPlayer.Name]; ok && teamPlayer.ID == val.ID {
-				val.Teams = append(val.Teams, teamDetails.Data.Team.Name)
-				players[teamPlayer.Name] = val
-			} else {
-				var teamPlayerDetails playerDetails
-				teamPlayerDetails.Age = teamPlayer.Age
-				teamPlayerDetails.ID = teamPlayer.ID
-				teamPlayerDetails.Teams = append(teamPlayerDetails.Teams, teamDetails.Data.Team.Name)
-				players[teamPlayer.Name] = teamPlayerDetails
-				playerNames = append(playerNames, teamPlayer.Name)
+		noOfTeamsFound++
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for _, teamPlayer := range apiResponse.Data.TeamDetails.Players {
+				lock.RLock()
+				if val, ok := players[teamPlayer.Name]; ok && teamPlayer.ID == val.ID {
+					lock.RUnlock()
+					val.Teams = append(val.Teams, apiResponse.Data.TeamDetails.Name)
+					lock.Lock()
+					players[teamPlayer.Name] = val
+					lock.Unlock()
+				} else {
+					lock.RUnlock()
+					var teamPlayerDetails playerDetails
+					teamPlayerDetails.Age = teamPlayer.Age
+					teamPlayerDetails.ID = teamPlayer.ID
+					teamPlayerDetails.Teams = append(teamPlayerDetails.Teams, apiResponse.Data.TeamDetails.Name)
+					lock.Lock()
+					players[teamPlayer.Name] = teamPlayerDetails
+					lock.Unlock()
+					playerNames = append(playerNames, teamPlayer.Name)
+				}
 			}
-		}
+		}()
 	}
+	wg.Wait()
 	cl := collate.New(language.English, collate.Loose)
 	cl.SortStrings(playerNames)
+	lock.RLock()
 	for index, playerName := range playerNames {
-		println(strconv.Itoa(index) + ". " + playerName + "; " + players[playerName].Age + "; " + strings.Join(players[playerName].Teams, ", "))
+		println(strconv.Itoa(index+1) + ". " + playerName + "; " + players[playerName].Age + "; " + strings.Join(players[playerName].Teams, ", "))
 	}
+	lock.RUnlock()
 }
 
+// contains returns true if the string is in the array, else returns false
 func contains(arr []string, str string) bool {
 	for _, a := range arr {
 		if a == str {
